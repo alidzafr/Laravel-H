ac7a1b66d4711bcbfc4128e23aaa8b5e8714c8fd
alidzafr <alidzafathur@gmail.com>
2025-11-28 12:50:46 +0700

    register (checkpoin)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\http;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 
@@ -25,24 +26,39 @@ class AuthController extends Controller
             'email' => 'required|email',
             'password' => 'required|string'
         ]);
-
+        
         if (Auth::attempt($validated)) {
             $request->session()->regenerate();
-
+            
             return redirect()->route('index');
         }
-
+        
         throw ValidationException::withMessages([
             'credentials' => 'Sorry, incorrect credentials'
         ]);
     }
-
+    
     public function logout (Request $request)
     {
         Auth::logout();
-
+        
         $request->session()->invalidate();
         $request->session()->regenerateToken();
+        
+        return redirect()->route('index');
+    }
+    
+    public function register (Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+
+        $user = User::create($validated);
+
+        Auth::login($user);
 
         return redirect()->route('index');
     }
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+    <title>Home</title>
+  </head>
+  <body>
+    <!-- Container -->
+    <section id="content">
+      <div class="flex justify-center items-center w-screen h-screen bg-zinc-50 ">
+        <!-- Card Container -->
+        <div class="relative flex flex-col m-6 space-y-10 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
+          <!-- Left Side -->
+          <div class="p-6 md:p-20">
+
+            <form action="{{ route('register') }}" method="POST">
+              @csrf
+              <!-- Top -->
+              <h2 class="mb-5 text-4xl font-bold">Log In</h2>
+              <p class="max-w-sm mb-10 font-light text-gray-600">
+                Log in to your account to Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <div class="flex flex-col space-y-2">
+                <input 
+                type="text"
+                name="name"
+                required
+                class="p-3 border border-gray-200 rounded-md placeholder:font-light"
+                placeholder="Enter your name">
+                
+                <input 
+                type="email"
+                name="email"
+                required
+                class="p-3 border border-gray-200 rounded-md placeholder:font-light"
+                placeholder="Enter your email address">
+  
+                <input 
+                type="password" 
+                name="password"
+                required
+                class="p-3 border border-gray-200 rounded-md placeholder:font-light"
+                placeholder="Enter password">
+  
+                <input 
+                type="password" 
+                name="password_confirmation"
+                required
+                class="p-3 border border-gray-200 rounded-md placeholder:font-light"
+                placeholder="Enter password">
+              </div>
+              
+              {{-- Validation Errors --}}
+              @if ($errors->any())
+              <ul class="px-4 py-2 my-2 bg-red-100">
+                @foreach ($errors->all() as $error)
+                <li class="my-2 text-red-500">{{ $error }}</li>
+                @endforeach
+              </ul>
+              @endif
+              
+              <!-- Bottom -->
+              <div class="flex flex-col items-center justify-between mt-6 space-y-6 md:flex-row md:space-y-0">
+                <a href="#" class="text-cyan-700">Forgot Password ?</a>
+                
+                <button 
+                type="submit" 
+                class="w-full md:w-auto flex justify-center items-center px-9 py-3 space-x-4 font-sans font-bold text-white rounded-md shadow-lg bg-cyan-400 shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150">
+                  <span>Next</span>
+                </button>
+              </div>
+
+            </form>
+
+            <div class="flex flex-col border-t border-gray-300 mt-6 space-y-6 md:flex-row md:space-y-0">
+              <a 
+              href="{{ route('register.show') }}" 
+              class="w-full md:w-auto flex justify-center mx-auto px-9 py-3 my-6 font-sans font-bold text-white rounded-md shadow-lg bg-cyan-400 shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150">
+                <span>Create New Account</span>
+              </a>
+            </div>
+          </div>
+          
+          <!-- Right Side -->
+          <img src="{{ asset('images/image.jpg')}}" alt="" class="w-[350px] hidden md:block" />
+
+
+        </div>
+      </div>
+    </section>
+    
+    
+  </body>
+</html>
