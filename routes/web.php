c2456db6d70dced37c6aa54ec42f2ed14a29d323
alidzafr <alidzafathur@gmail.com>
2025-12-21 15:04:11 +0700

    employee and skill table

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register.show');
@@ -9,6 +10,8 @@ Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+Route::get('/employee', [EmployeeController::class, 'index'])->name('employee.index');
+
 Route::get('/', function () {
-    return view('app');
+    return view('welcome');
 })->name('index');
